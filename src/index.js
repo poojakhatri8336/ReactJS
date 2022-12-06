53f8278c7e7ad0ee54742715b206dfdbd5c04ef5
poojakhatri8336 <71308472+poojakhatri8336@users.noreply.github.com>
2022-12-06 12:39:42 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from 'react-dom';
+
+//ReactDOM.render(what to show, where to show )
+
+ReactDOM.render(<div><h1> Hello There! Learning React </h1><p>Hey</p></div>,
+   document.getElementById("root"));
+
